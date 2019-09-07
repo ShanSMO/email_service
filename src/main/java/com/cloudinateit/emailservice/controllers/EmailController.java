79de56fd35f90485f07c3ad8db98695b8710c05c
shanaka <madushanka991@gmail.com>
2019-09-07 21:16:01 +0530

    Changes

--- src/main/java/com/cloudinateit/emailservice/controllers/EmailController.java
+++ src/main/java/com/cloudinateit/emailservice/controllers/EmailController.java
@@ -4,12 +4,10 @@ import com.cloudinateit.emailservice.dtos.ResponseDto;
 import com.cloudinateit.emailservice.models.Email;
 import com.cloudinateit.emailservice.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "email")
@@ -18,10 +16,18 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
+    @Value("${name}")
+    private String userName;
+
     @RequestMapping(value = "send", method = RequestMethod.POST)
     public ResponseEntity<ResponseDto> sendMail(@RequestBody Email email) {
         ResponseDto responseDto = emailService.sendMail(email);
         return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "test-config")
+    public String testConfig() {
+        return  userName;
     }
 
 }
